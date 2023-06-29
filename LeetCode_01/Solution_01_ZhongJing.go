2dc90052a3498cfdff139006aec3151b75aea01b
zhongjing0121 <zhongjing0121@aliyun.com>
2023-06-29 22:54:39 +0800

    2023-06-29 添加LeetCode01

---
+++ LeetCode_01/Solution_01_ZhongJing.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+作者：仲景
+日期：2023-06-29
+*/
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for index, num := range nums {
+		success := target - num
+		if v, exists := m[success]; exists {
+			return []int{v, index}
+		} else {
+			m[num] = index
+		}
+	}
+
+	return nil
+}
+
