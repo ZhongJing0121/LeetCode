b9d80aa1a53138d26cae03d792f2fbb99915a4fb
zhongjing <zhongjing0121@aliyun.com>
2023-06-30 22:52:28 +0800

    修改 修改提交文件名、合并多个题解为一个文件

--- LeetCode_01/Solution_ZhongJing.go
+++ LeetCode_01/Solution_ZhongJing.go
@@ -17,4 +17,3 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
-
--- LeetCode_2490/Solution_ZhongJing.go
+++ LeetCode_2490/Solution_ZhongJing.go
@@ -2,6 +2,10 @@ package main
 
 import "strings"
 
+/**
+作者：仲景
+日期：2023-06-30
+*/
 func isCircularSentence(sentence string) bool {
 	sLen := len(sentence)
 	if sentence[0] != sentence[sLen-1] {
@@ -20,5 +24,20 @@ func isCircularSentence(sentence string) bool {
 		preEnd = arr[i][len(arr[i])-1]
 	}
 
+	return true
+}
+
+func isCircularSentence02(sentence string) bool {
+	sLen := len(sentence)
+
+	if sentence[0] != sentence[sLen-1] {
+		return false
+	}
+
+	for i := 0; i < sLen; i++ {
+		if sentence[i] == ' ' && sentence[i+1] != sentence[i-1] {
+			return false
+		}
+	}
 	return true
 }
