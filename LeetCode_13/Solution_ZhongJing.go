47d66d7673ba31bbd4eefe04ba43eab4d07c3e2c
zhongjing <zhongjing0121@aliyun.com>
2023-06-30 23:19:50 +0800

    新增 2023-06-30 LeetCode13. 罗马数字转整数 Go语言题解

---
+++ LeetCode_13/Solution_ZhongJing.go
@@ -0,0 +1,26 @@
+package main
+
+var symbolValues = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+// 作者：仲景
+// 日期：2023-06-30
+func romanToInt(s string) (ans int) {
+	n := len(s)
+	for i := range s {
+		value := symbolValues[s[i]]
+		if i < n-1 && value < symbolValues[s[i+1]] {
+			ans -= value
+		} else {
+			ans += value
+		}
+	}
+	return
+}
