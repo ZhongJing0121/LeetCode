11a085f82225b0151efc7b3754f02df5bcc9d3a5
zhongjing <zhongjing0121@aliyun.com>
2023-07-01 23:46:08 +0800

    新增 26.删除有序数组中的重复项 Go语言题解

---
+++ LeetCode_26/Solution_ZhongJing.go
@@ -0,0 +1,17 @@
+package main
+
+// 日期：2023-07-01
+// 作者：仲景
+func removeDuplicates(nums []int) int {
+	index, numsLen := 1, len(nums)
+	if numsLen == 0 {
+		return 0
+	}
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
+}
