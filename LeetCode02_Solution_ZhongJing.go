5107c612de427d6b31048b612457b718449c8f37
zhongjing <zhongjing0121@aliyun.com>
2023-07-02 13:35:54 +0800

    新增 2.两数相加 Go语言题解

---
+++ LeetCode02_Solution_ZhongJing.go
@@ -0,0 +1,39 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 日期：2023-07-02
+// 作者：仲景
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	newHead := &ListNode{}
+	node, node1, node2 := newHead, l1, l2
+	num := 0
+
+	for node1 != nil || node2 != nil {
+		val1, val2 := 0, 0
+		if node1 != nil {
+			val1 = node1.Val
+			node1 = node1.Next
+		}
+		if node2 != nil {
+			val2 = node2.Val
+			node2 = node2.Next
+		}
+
+		sum := num + val1 + val2
+		cur := sum % 10
+		num = sum / 10
+
+		node.Next = &ListNode{Val: cur}
+		node = node.Next
+	}
+
+	if num > 0 {
+		node.Next = &ListNode{Val: num}
+	}
+
+	return newHead.Next
+}
