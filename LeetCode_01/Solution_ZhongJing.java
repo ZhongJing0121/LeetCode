fabe3de87af08cf3b8d2b6314c09c31f183d8b17
zhongjing <zhongjing0121@aliyun.com>
2023-07-02 17:50:18 +0800

    新增 LeetCode01 Java Kt题解

---
+++ LeetCode_01/Solution_ZhongJing.java
@@ -0,0 +1,23 @@
+package LeetCode_01;
+
+import java.util.HashMap;
+
+/**
+ * 日期：20223-07-01
+ * 作者：仲景
+ */
+public class Solution_ZhongJing {
+    public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            var curTarget = target - nums[i];
+            if (map.containsKey(curTarget)) {
+                return new int[]{map.get(curTarget), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return null;
+    }
+}
---
+++ LeetCode_01/Solution_ZhongJing.kt
@@ -0,0 +1,17 @@
+package LeetCode_01
+
+/**
+ * 日期：2023-07-01
+ * 作者：仲景
+ */
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val m = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, num ->
+            val curTarget = target - num
+            m[curTarget]?.let { return intArrayOf(index, it) }
+            m[num] = index
+        }
+        return intArrayOf()
+    }
+}
