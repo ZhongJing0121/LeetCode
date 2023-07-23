7bd86108e228cddd6c6dd6c50ad11199c9e5aab3
zhongjing <zhongjing0121@aliyun.com>
2023-07-24 00:33:18 +0800

    新增 2023-07-17每日一题 860 Java题解

---
+++ LeetCode_860/Solution01_ZhongJing.java
@@ -0,0 +1,31 @@
+package LeetCode_860;
+
+/**
+ * 题目：860. 柠檬水找零
+ * 日期：2023-07-22
+ * 作者：仲景
+ */
+public class Solution01_ZhongJing {
+    public boolean lemonadeChange(int[] bills) {
+        int fiveCount = 0, tenCount = 0;
+        for (int money : bills) {
+            if (money == 5) {
+                fiveCount++;
+            } else if (money == 10) {
+                fiveCount--;
+                tenCount++;
+            } else if (tenCount > 0) {
+                tenCount--;
+                fiveCount--;
+            } else {
+                fiveCount -= 3;
+            }
+
+            if (fiveCount < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
