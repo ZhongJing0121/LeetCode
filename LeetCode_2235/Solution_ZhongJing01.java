bc94d822bee8673f6b8f0b312b7a3214254e5881
zhongjing <zhongjing0121@aliyun.com>
2023-08-21 11:12:54 +0800

    新增 LC-2235.两整数相加 Java题解

---
+++ LeetCode_2235/Solution_ZhongJing01.java
@@ -0,0 +1,11 @@
+package LeetCode_2235;
+
+/**
+ * @author ZhongJing </p>
+ * @date 2023/08/21 </p>
+ */
+public class Solution_ZhongJing01 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
