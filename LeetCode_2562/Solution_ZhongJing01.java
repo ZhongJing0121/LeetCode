e863cdc35bfe48f01f70a3e451a810f169c50a8b
zhongjing <zhongjing0121@aliyun.com>
2023-10-12 17:22:12 +0800

    2562.找出数组的串联值

---
+++ LeetCode_2562/Solution_ZhongJing01.java
@@ -0,0 +1,34 @@
+package LeetCode_2562;
+
+/**
+ * 2562.找出数组的串联值
+ *
+ * @author ZhongJing </p>
+ * @date 2023/10/12 </p>
+ */
+public class Solution_ZhongJing01 {
+    public long findTheArrayConcVal(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        long res = 0;
+
+        while (left < right) {
+            int rightNum = nums[right];
+            int leftNum = nums[left];
+            int size = 10;
+            while (size <= rightNum) {
+                size *= 10;
+            }
+            res += (long) leftNum * size + rightNum;
+            right--;
+            left++;
+        }
+
+        // 处理数组长度为奇数的情况
+        if (left == right) {
+            res += nums[left];
+        }
+
+        return res;
+    }
+}
