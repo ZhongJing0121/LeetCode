063312eeed2db74148ac6b84fd558fd5fa94fe9a
zhongjing <zhongjing0121@aliyun.com>
2023-10-18 15:32:55 +0800

    136. 只出现一次的数字 Java

---
+++ LeetCode_136/Solution_ZhongJing.java
@@ -0,0 +1,17 @@
+package LeetCode_136;
+
+/**
+ * 136. 只出现一次的数字
+ *
+ * @author ZhongJing </p>
+ * @date 2023/10/18 </p>
+ */
+public class Solution_ZhongJing {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
