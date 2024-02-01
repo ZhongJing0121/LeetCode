833a56dd16027664f0ed5798d9a82c5674d8abb5
zhongjing <zhongjing0121@aliyun.com>
2024-02-01 10:01:42 +0800

    2670.找出不同元素数目差数组

---
+++ LeetCode_2670/Solution_ZhongJing01.java
@@ -0,0 +1,41 @@
+package LeetCode_2670;
+
+import java.util.HashSet;
+
+/**
+ * 2670.找出不同元素数目差数组
+ *
+ * @author ZhongJing </p>
+ * @date 2024/01/31 </p>
+ */
+public class Solution_ZhongJing01 {
+    public int[] distinctDifferenceArray(int[] nums) {
+        // 构建前缀数组
+        var prefixArray = new int[nums.length];
+
+        // 记录出现的数字次数
+        var set = new HashSet<Integer>();
+
+        // 从前往后遍历数组
+        for (int i = 0; i < nums.length; i++) {
+            // 前缀包含当前数，先add再赋值
+            set.add(nums[i]);
+            prefixArray[i] = set.size();
+        }
+
+        // 构建后缀数组
+        var res = new int[nums.length];
+        // 清空set
+        set.clear();
+        // 从后往前遍历数组
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // 结果等于前缀-后缀
+            res[i] = prefixArray[i] - set.size();
+            // 后缀不包含当前数，先赋值再add
+            set.add(nums[i]);
+        }
+
+        return res;
+    }
+
+}
