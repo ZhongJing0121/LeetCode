738a127cca5fcc1bf040ac9efb4cae320522fd56
zhongjing <zhongjing0121@aliyun.com>
2024-04-23 14:24:19 +0800

    2469.温度转换  Java

---
+++ LeetCode_2469/Solution_ZhongJing01.java
@@ -0,0 +1,10 @@
+package LeetCode_2469;
+
+/**
+ * 2469.温度转换
+ */
+public class Solution_ZhongJing01 {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
