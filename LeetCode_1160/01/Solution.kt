3870886478136a5a67ffc2a83b22f5dc870b00d1
zhongjing <zhongjing0121@aliyun.com>
2024-08-24 01:00:37 +0800

    1160.拼写单词

---
+++ LeetCode_1160/01/Solution.kt
@@ -0,0 +1,30 @@
+package LeetCode_1160.`01`
+
+/**
+ * 1160.拼写单词
+ */
+class Solution {
+    fun countCharacters(words: Array<String>, chars: String): Int {
+        val charsCountMap = getStrCharMap(chars)
+        var res = 0
+        words.forEach { word ->
+            val wordCharCountMap = getStrCharMap(word)
+            for (i in 0..26) {
+                if (wordCharCountMap[i] > charsCountMap[i]) return@forEach
+            }
+            res += word.length
+        }
+        return res
+    }
+
+    /**
+     * 获取字符串中每个字符出现次数,返回Map
+     */
+    fun getStrCharMap(s: String): IntArray {
+        val res = IntArray(27)
+        s.forEach { c ->
+            res[c - 'a']++
+        }
+        return res
+    }
+}
