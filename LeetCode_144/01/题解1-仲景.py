c5b0fd893637ab68c41fbd2396c2cf35e1802b0a
zhongjing <zhongjing0121@aliyun.com>
2024-09-26 21:03:36 +0800

    144.二叉树的前序遍历

---
+++ LeetCode_144/01/题解1-仲景.py
@@ -0,0 +1,25 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    144.二叉树的前序遍历
+    """
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = [root] if root else []
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.right:
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+        return res
---
+++ LeetCode_3208/01/Solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    """
+    3208.交替组 II
+    """
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
