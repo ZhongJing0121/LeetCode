f457223acd6ec804f5bdca0114203f51707d8f98
zhongjing <zhongjing0121@aliyun.com>
2024-11-25 17:31:30 +0800

    2180.统计各位数字之和为偶数的整数个数

---
+++ LeetCode_2180/solution_01/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    2180.统计各位数字之和为偶数的整数个数
+    Description:
+    Author: 仲景
+    Date: 2024/11/25
+    """
+
+    def countEven(self, num: int) -> int:
+        res = 0
+        for i in range(1, num + 1):
+            curNumBitSum = self.numBitSum(i)
+            res += 1 if curNumBitSum % 2 == 0 else 0
+        return res
+
+    def numBitSum(self, num: int) -> int:
+        """
+        求数字各位的总和
+        """
+        sum = 0
+        while num:
+            sum += num % 10
+            num //= 10
+        return sum
