5166324e94fbacd910a1b44e03bd0f7b1e1040eb
zhongjing <zhongjing0121@aliyun.com>
2025-07-29 23:40:28 +0800

    fix(2798. 满足目标工作时长的员工数目):更新题解

--- LeetCode_2798/01/solution.py
+++ LeetCode_2798/01/solution.py
@@ -1,11 +1,13 @@
 from typing import List
 
 
+# noinspection PyMethodMayBeStatic
 class Solution:
     """
     题目：2798. 满足目标工作时长的员工数目
     链接：https://leetcode.cn/problems/number-of-employees-who-met-the-target/description/
     思路：没啥好说的
     """
+
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        return sum(1 for hour in hours if hour >= target)
+        return len([1 for hour in hours if hour >= target])
